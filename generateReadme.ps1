a5a01e0a2310e423f3e5a930c994796216f6fde8
Van Gulick Stéphane <Stephane.VanGulick@swisscom.com>
2017-10-20 10:35:44 +0200

    Added generateReadme.ps1

---
+++ generateReadme.ps1
@@ -0,0 +1,37 @@
+
+$a = @"
+# Django learning repo
+
+This repository contains my files, and notes of my learning path into the Django framework.
+
+## Ressources used:
+
+### videos
+
+[The New Boston video tutorials](https://www.youtube.com/watch?v=qgGIqRFvFFk&list=PL6gx4Cwl9DGBlmzzFcLgDhKTTfNLfX1IK)
+
+### blog posts
+tbd
+
+### cheat cheets
+tbd
+
+### books
+
+tbd
+
+## Learning path
+
+"@
+
+$AllMDs = Get-ChildItem .\ -Recurse -Filter "*.md"
+
+foreach ($file in $AllMDs){
+    $relPath = $null
+    $relPath = $file | Resolve-Path -Relative
+    $a += "[$($file.BaseName)]($relPath)"
+    $a += "<br>"
+
+}
+
+$a > readme.md
