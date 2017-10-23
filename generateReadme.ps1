dabef0802c4ac9cff2290744f41f5bcd9073263f
Van Gulick Stéphane <Stephane.VanGulick@swisscom.com>
2017-10-23 07:09:58 +0200

    Updated Readme

--- generateReadme.ps1
+++ generateReadme.ps1
@@ -4,21 +4,22 @@ $a = @"
 
 This repository contains my files, and notes of my learning path into the Django framework.
 
-## Ressources used:
+## Learning Ressources:
 
 ### videos
 
-[The New Boston video tutorials](https://www.youtube.com/watch?v=qgGIqRFvFFk&list=PL6gx4Cwl9DGBlmzzFcLgDhKTTfNLfX1IK)
-
-### blog posts
-tbd
+- [The New Boston video tutorials](https://www.youtube.com/watch?v=qgGIqRFvFFk&list=PL6gx4Cwl9DGBlmzzFcLgDhKTTfNLfX1IK)
+- [Derek Banas](https://www.youtube.com/playlist?list=PLGLfVvz_LVvSMqZiTTsAC7C8Ypp81Jt6D)
 
 ### cheat cheets
-tbd
 
-### books
+- [Mercury Tide](https://www.mercurytide.co.uk/media/resources/django-cheat-sheet.pdf)
+- [RevSys](http://media.revsys.com/images/django-1.5-cheatsheet.pdf)
 
-tbd
+### books I used
+
+- [Two scoops of Django](https://www.amazon.fr/Two-Scoops-Django-1-11-Practices/dp/0692915729/ref=sr_1_2?ie=UTF8&qid=1508575688&sr=8-2&keywords=django+python)
+
 
 ## Learning path
 
